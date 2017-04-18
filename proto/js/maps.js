11c5cece901c2dc22fd21db78e9ed5cd9a22866a
rajesh peddalachugari <rajesh.peddalachugari@gmail.com>
2017-04-18 09:58:03 +0530

    proto

---
+++ proto/js/maps.js
@@ -0,0 +1,94 @@
+/// <reference path="jquery-3.2.0.min.js" />
+
+
+//var x = document.getElementById("demo");
+//window.onload = function (e) {
+//    getLocation();
+//};
+//function getLocation() {
+//    if (navigator.geolocation) {
+//        navigator.geolocation.getCurrentPosition(showPosition, showError);
+//    } else {
+//        x.innerHTML = "Geolocation is not supported by this browser.";
+//    }
+//}
+
+//function showPosition(position) {
+//    var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+//    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="
+//    + latlon + "&zoom=14&size=400x300&sensor=false&key=AIzaSyAUpb6jaGl9tnqkV_-73hzQfMc20NY3rpQ";
+//    document.getElementById("mapholder").innerHTML = "<img src='" + img_url + "'>";
+//}
+////To use this code on your website, get a free API key from Google.
+////Read more at: https://www.w3schools.com/graphics/google_maps_basic.asp
+
+//function showError(error) {
+//    switch (error.code) {
+//        case error.PERMISSION_DENIED:
+//            x.innerHTML = "User denied the request for Geolocation."
+//            break;
+//        case error.POSITION_UNAVAILABLE:
+//            x.innerHTML = "Location information is unavailable."
+//            break;
+//        case error.TIMEOUT:
+//            x.innerHTML = "The request to get user location timed out."
+//            break;
+//        case error.UNKNOWN_ERROR:
+//            x.innerHTML = "An unknown error occurred."
+//            break;
+//    }
+//}
+
+
+
+window.onload = function (e) {
+    getLocation();
+};
+
+$(".init").click(function () {
+    $(".splashScreen").fadeOut("slow");
+});
+
+var x = document.getElementById("demo");
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    }
+    else { x.innerHTML = "Geolocation is not supported by this browser."; }
+}
+
+function showPosition(position) {
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+    latlon = new google.maps.LatLng(lat, lon)
+    mapholder = document.getElementById('mapholder')
+    mapholder.style.height = '62vh';
+    mapholder.style.width = '100%';
+
+    var myOptions = {
+        center: latlon, zoom: 14,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: false,
+        navigationControlOptions: { style: google.maps.NavigationControlStyle.SMALL }
+    };
+    var map = new google.maps.Map(document.getElementById("mapholder"), myOptions);
+    var marker = new google.maps.Marker({ position: latlon, map: map, title: "You are here!" });
+}
+
+function showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred."
+            break;
+    }
+}
